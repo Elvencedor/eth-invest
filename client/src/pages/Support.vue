7abc72111ca17948f3e5768e4780a314f85c096e
Idongesit Ntuk <ideeofficial@gmail.com>
2020-04-16 05:24:41 +0100

    Update support page

--- client/src/pages/Support.vue
+++ client/src/pages/Support.vue
@@ -4,19 +4,19 @@
     <div class="w-full max-w-xl text-center mt-10">
       <div class="flex flex-wrap">
         <div class="w-1/3">
-          <a href="http://t.me/CrespoCSO" class="block border-2 border-red-500 rounded m-4 p-4 text-white text-center text-sm hover:bg-red-500 hover:text-gray-900" title="Telegram">
+          <a href="http://t.me/CrespoCSO" class="block border-2 border-green-500 rounded m-4 p-4 text-green-500 text-center text-sm hover:bg-green-500 hover:text-gray-900" title="Telegram">
             <i class="fab fa-telegram fa-2x"></i>
             <div class="mt-2">Telegram</div>
           </a>
         </div>
         <div class="w-1/3">
-          <a href="https://twitter.com/clubcrespo/" class="block border-2 border-red-500 rounded m-4 p-4 text-white text-center text-sm hover:bg-red-500 hover:text-gray-900" title="Twitter">
+          <a href="https://twitter.com/clubcrespo/" class="block border-2 border-green-500 rounded m-4 p-4 text-green-500 text-center text-sm hover:bg-green-500 hover:text-gray-900" title="Twitter">
             <i class="fab fa-twitter fa-2x"></i>
             <div class="mt-2">Twitter</div>
           </a>
         </div>
         <div class="w-1/3">
-          <a href="mailto:cresposachi@gmail.com" class="block border-2 border-red-500 rounded m-4 p-4 text-white text-center text-sm hover:bg-red-500 hover:text-gray-900" title="Mail">
+          <a href="mailto:cresposachi@gmail.com" class="block border-2 border-green-500 rounded m-4 p-4 text-green-500 text-center text-sm hover:bg-green-500 hover:text-gray-900" title="Mail">
             <i class="fas fa-at fa-2x"></i>
             <div class="mt-2">Mail</div>
           </a>
@@ -24,24 +24,24 @@
       </div>
     </div>
     <div class="w-full max-w-xl">
-      <form @submit.prevent="submit" class="bg-gray-800 py-6 rounded-lg mt-10">
+      <form @submit.prevent="submit" class="border-2 py-6 rounded-lg mt-10">
         <div class="flex flex-wrap">
           <div class="w-full sm:w-1/2 px-6">
             <label
               for="name"
-              class="text-white"
+              class="text-gray-900"
             >
               Your Name
             </label>
             <input 
               type="text" 
               v-model="$v.form.name.$model" 
-              class="mt-3 w-full rounded-lg py-3 px-6 bg-gray-900 text-white" 
+              class="mt-3 w-full rounded-lg py-3 px-6 bg-gray-200 text-gray-600" 
               id="name" 
               placeholder=""
             >
             <p 
-              class="text-xs text-red-500 italic" 
+              class="text-xs text-green-500 italic" 
               v-if="$v.form.name.$error && !$v.form.name.required"
             >
               Name is required
@@ -50,19 +50,19 @@
           <div class="w-full sm:w-1/2 px-6 mt-6 sm:mt-0">
             <label 
               for="email" 
-              class="text-white"
+              class="text-gray-900"
             >
               Your E-mail
             </label>
             <input 
               type="email" 
               v-model="$v.form.email.$model" 
-              class="mt-3 w-full rounded-lg py-3 px-6 bg-gray-900 text-white" 
+              class="mt-3 w-full rounded-lg py-3 px-6 bg-gray-200 text-gray-600" 
               id="email" 
               placeholder=""
             >
             <p 
-              class="text-xs text-red-500 italic" 
+              class="text-xs text-green-500 italic" 
               v-if="$v.form.email.$error && !$v.form.email.required"
             >
               E-mail is required
@@ -73,14 +73,14 @@
           <div class="px-6">
             <label 
               for="topic" 
-              class="text-white"
+              class="text-gray-900"
             >
               What can we do for you?
             </label>
             <select
               id="topic"
               v-model="$v.form.topic.$model" 
-              class="mt-3 w-full rounded-lg py-3 px-6 bg-gray-900 text-white" 
+              class="mt-3 w-full rounded-lg py-3 px-6 bg-gray-200 text-gray-600" 
             >
               <option value="" selected disabled>Choose topic</option>
               <option value="deposit">Deposit</option>
@@ -90,7 +90,7 @@
               <option value="other">Other (describe below)</option>
             </select>
             <p 
-              class="text-xs text-red-500 italic" 
+              class="text-xs text-green-500 italic" 
               v-if="$v.form.topic.$error && !$v.form.topic.required"
             >
               Topic is required
@@ -101,7 +101,7 @@
           <div class="px-6">
             <label 
               for="customTopic" 
-              class="text-white"
+              class="text-gray-900"
             >
               Specify topic
             </label>
@@ -109,12 +109,12 @@
              <input 
               type="text" 
               v-model="$v.form.customTopic.$model" 
-              class="mt-3 w-full rounded-lg py-3 px-6 bg-gray-900 text-white" 
+              class="mt-3 w-full rounded-lg py-3 px-6 bg-gray-200 text-gray-600" 
               id="customTopic" 
               placeholder="Specify nature of discussion"
             >
             <p 
-              class="text-xs text-red-500 italic" 
+              class="text-xs text-green-500 italic" 
               v-if="$v.form.customTopic.$error && !$v.form.customTopic.required"
             >
               Topic is required
@@ -125,20 +125,20 @@
           <div class="px-6">
             <label 
               for="description"
-              class="text-white"
+              class="text-gray-900"
             >
               What is your question, comment or issue?
             </label>
             <textarea 
               v-model="$v.form.description.$model" 
-              class="mt-3 w-full rounded-lg py-3 px-6 bg-gray-900 text-white" 
+              class="mt-3 w-full rounded-lg py-3 px-6 bg-gray-200 text-gray-600" 
               id="description" 
               rows="6" 
               placeholder=""
             >
             </textarea>
             <p 
-              class="text-red-500 text-xs italic" 
+              class="text-green-500 text-xs italic" 
               v-if="$v.form.description.$error && !$v.form.description.required"
             >
               Description is required
@@ -149,7 +149,7 @@
           <div class="px-6">
             <button
               type="submit"
-              class="mt-3 w-full shadow rounded-lg py-3 px-6 bg-red-500 hover:bg-red-400 disabled:opacity-50"
+              class="mt-3 w-full shadow rounded-lg py-3 px-6 bg-green-500 hover:bg-green-400 disabled:opacity-50"
               :disabled="loading"
             >
               Submit
