a82d8e586a48dfb281537d598afd487ac301533f
Idongesit Ntuk <ideeofficial@gmail.com>
2020-04-20 17:00:44 +0100

    fix router issue

--- client/server.js
+++ client/server.js
@@ -1,8 +1,14 @@
 const express = require("express");
+const history = require('connect-history-api-fallback')
 const serveStatic = require("serve-static")
 const path = require('path');
 
 const app = express();
+
+app.use(history({
+  // OPTIONAL: Includes more verbose logging
+  verbose: true
+}))
 
 app.use(serveStatic(path.join(__dirname, 'dist')));
 const port = process.env.PORT || 3000;
