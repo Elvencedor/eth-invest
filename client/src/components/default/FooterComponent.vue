c87d372750e1e4b5078e9f85fa4ec39cc6ff4ccd
Idongesit Ntuk <ideeofficial@gmail.com>
2020-05-09 14:03:29 +0100

    Modified social links

--- client/src/components/default/FooterComponent.vue
+++ client/src/components/default/FooterComponent.vue
@@ -7,8 +7,10 @@
       <div>
         <ul class="flex">
           <li><a href="http://t.me/CrespoCSO" class="text-xl mx-2 hover:text-red-500" title="Telegram"><i class="fab fa-telegram"></i></a></li>
-          <li><a href="https://twitter.com/clubcrespo/" class="text-xl mx-2 hover:text-red-500" title="Twitter"><i class="fab fa-twitter"></i></a></li>
-          <li><a href="mailto:cresposachi@gmail.com" class="text-xl ml-2 hover:text-red-500" title="Mail"><i class="fas fa-at"></i></a></li>
+          <li><a href="https://web.facebook.com/Eth-Invest-102500161459049" class="text-xl mx-2 hover:text-red-500" title="Facebook"><i class="fab fa-facebook"></i></a></li>
+          <li><a href="https://twitter.com/ethinvest_club" class="text-xl mx-2 hover:text-red-500" title="Twitter"><i class="fab fa-twitter"></i></a></li>
+          <li><a href="https://www.instagram.com/ethinvest_club" class="text-xl mx-2 hover:text-red-500" title="Twitter"><i class="fab fa-instagram"></i></a></li>
+          <li><a href="mailto:ethinvest.club@gmail.com" class="text-xl ml-2 hover:text-red-500" title="Mail"><i class="fas fa-at"></i></a></li>
         </ul>
       </div>
     </div>
